ff7c8d74b3bad0b49e5480641bdf90546db2a87a
gschwandtner <gschwandtner2k@protonmail.com>
2021-08-26 15:45:53 +0200

    fixed comments

--- script/run.sh
+++ script/run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/sh
 # with test options
 # (unfinished)
+# STDERR > Proxmox log
+# STDOUT > Proxmox answer
 
 # --- USER CONFIG ---
-LOG="true"                      # true for enabeling log
+
+LOG="true"                      # true for enabling log
 LOG_PATH="/home/me/temp"        # should be absolute path
 
 IP="192.168.56.101:80"          # ip + port
@@ -12,17 +15,14 @@ FILE="../test_files/Ntr7x12i"   # should be absolute path
 
 # --- TEST START ---
 # arguments for testing
-[ "$1" = "-n" ] && IP="192.168.56.101:3182"
 
 [ "$1" = "-v" ] && FILE="../test_files/eF9f8G3f"
+[ "$1" = "-n" ] && IP="192.168.56.0:3182"
+[ "$1" = "-h" ] && echo "-v virustest" && echo "-n nettest" && exit 0
 
-if [ "$1" = "-h" ]
-    then
-    echo "-v virustest"
-    echo "-n nettest"
-fi
+# --- CURL ---
+# run the curl command and store xml entry in variables + print curl error
 
-# --- RUN CURL ---
 OUTPUT=$(curl -K ./curl.conf http://$IP/virusscan -F up_file=@$FILE 2> $LOG_PATH/ikarus-server_pmg.temp.log)
 ERROR=$(cat $LOG_PATH/ikarus-server_pmg.temp.log)
 rm $LOG_PATH/ikarus-server_pmg.temp.log
@@ -31,29 +31,26 @@ rm $LOG_PATH/ikarus-server_pmg.temp.log
 STATUS=$(echo $OUTPUT | grep -o -P '(?<=<status>).*(?=</status>)')
 NAME=$(echo $OUTPUT | grep -o -P '(?<=<name>).*(?=</name>)')
 
-#echo $STATUS
-# >> clean / infected
+# --- PRINT ---
+# print output to STDOUT
 
-# if OUTPUT null
-# >> OK + command(?)
+# if OUTPUT null >> OK + command(?)
 [ -z "$OUTPUT" ] && echo "OK" #&& command when server offline
 
-# if NAME !null
-# >> VIRUS: + name from XML
+# if NAME !null >> VIRUS: + name from XML
 [ -n "$NAME" ] && echo "VIRUS: $NAME"
 
-# if OUTPUT !null & NAME null
-# >> OK
+# if OUTPUT !null & NAME null >> OK
 [ -n "$OUTPUT" ] && [ -z "$NAME" ] && echo "OK"
 
 # --- LOG ---
-# if LOG true
+# if $LOG true
+
 # date; filename; status >> LOG_PATH/ikarus-server_pmg.log
 if [ "$LOG" = "true" ]
     then
     echo `date` >> $LOG_PATH/ikarus-server_pmg.log
-    # check if OUTPUT null
-    # >> info or network error
+    # check if OUTPUT null >> info or network error
     if [ -n "$OUTPUT" ]
         then
         echo "FILE NAME" >> $LOG_PATH/ikarus-server_pmg.log
@@ -63,6 +60,6 @@ if [ "$LOG" = "true" ]
         echo "FILE NAME" >> $LOG_PATH/ikarus-server_pmg.log
         echo "$ERROR" >> $LOG_PATH/ikarus-server_pmg.log
     fi
-    # seperator for different entrys
+    # separator for different entries
     echo "---" >> $LOG_PATH/ikarus-server_pmg.log
 fi
