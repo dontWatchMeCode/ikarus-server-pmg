40ed1319f2eedb7a7c6b954dc1bb4a98348e0d12
gschwandtner <gschwandtner2k@protonmail.com>
2021-08-26 11:12:55 +0200

    added docu and moved to /tmp folder

--- install/debian_based.sh
+++ install/debian_based.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+# debian / ubuntu install script for the ikarus server
 
 echo "this will only work on ubuntu/debian based distors"
 
@@ -8,7 +9,8 @@ URL='https://fx.ikarus.at/scan.server/'
 [ $(uname -m) = "x64_32" ] && NAME=$(wget -qO- $URL | grep -m 1 -o -E "ikarus-scanserver[^<>]*?i386.deb" | head -1)
 
 PATH="${URL}${NAME}"
+cd /tmp
 /usr/bin/wget $PATH
 
-apt install ./ikarus-scanserver*
-rm ./ikarus-scanserver*
+apt install /tmp/ikarus-scanserver*
+rm /tmp/ikarus-scanserver*
