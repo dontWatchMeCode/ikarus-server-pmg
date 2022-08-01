ab2bf89cff83c3883cd9ad1e23c880263f8fbf30
M. Gschwandtner <84477901+OnlyPain-ctrl@users.noreply.github.com>
2022-08-01 22:31:07 +0200

    small fixes to install script

--- install/debian_based.sh
+++ install/debian_based.sh
@@ -5,12 +5,12 @@ echo "this will only work on ubuntu/debian based distors"
 
 URL='https://fx.ikarus.at/scan.server/'
 
-[ $(uname -m) = "x86_64" ] && NAME=$(wget -qO- $URL | grep -m 1 -o -E "ikarus-scanserver[^<>]*?amd64.deb" | head -1)
-[ $(uname -m) = "x64_32" ] && NAME=$(wget -qO- $URL | grep -m 1 -o -E "ikarus-scanserver[^<>]*?i386.deb" | head -1)
+[ "$(uname -m)" = "x86_64" ] && NAME=$(wget -qO- $URL | grep -m 1 -o -E "ikarus-scanserver[^<>]*?amd64.deb" | head -1)
+[ "$(uname -m)" = "x64_32" ] && NAME=$(wget -qO- $URL | grep -m 1 -o -E "ikarus-scanserver[^<>]*?i386.deb" | head -1)
 
 PATH="${URL}${NAME}"
-cd /tmp
-/usr/bin/wget $PATH
+cd /tmp &&
+/usr/bin/wget "$PATH"
 
 apt install /tmp/ikarus-scanserver*
 rm /tmp/ikarus-scanserver*
